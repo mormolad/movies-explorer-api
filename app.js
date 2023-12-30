c29099499af912186166ecc6830284f85b4327a9
mormolad <mormolad@yandex.ru>
2023-12-31 01:08:38 +0700

    fix CORS

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(
       "http://localhost:3000",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "origin", "Authorization"],
   })
 );
 app.use(bodyParser.json());
