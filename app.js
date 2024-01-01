c3b711a52dc145d2ea304b265f4a0806201393b2
Perevalov Anton <mormolad@yandex.ru>
2024-01-02 02:13:20 +0700

    cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const dotenv = require("dotenv").config();
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const { errors } = require("celebrate");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
@@ -10,7 +10,7 @@ const { limiter } = require("./utils/limiter");
 const router = require("./routes/index");
 const { sendError } = require("./utils/handlerErrors");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-const cors = require("./middlewares/cors");
+//const cors = require("./middlewares/cors");
 
 const { PORT = 3000, MONGODB_URL = "mongodb://127.0.0.1:27017/bitfilmsdb" } =
   process.env;
@@ -26,22 +26,21 @@ mongoose
 const app = express();
 
 app.use(
-  cors
-  // cors({
-  //   credentials: true,
-  //   origin: [
-  //     "http://filmissio.nomoredomainsmonster.ru/",
-  //     "https://filmissio.nomoredomainsmonster.ru/",
-  //     "http://api.filmissio.nomoredomainsmonster.ru/",
-  //     "https://api.filmissio.nomoredomainsmonster.ru/",
-  //     "http://localhost:3001",
-  //     "http://localhost:3000",
-  //   ],
-  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  //   allowedHeaders: ["Content-Type", "origin", "Authorization"],
-  // })
+  cors({
+    credentials: true,
+    origin: [
+      "http://filmissio.nomoredomainsmonster.ru/",
+      "https://filmissio.nomoredomainsmonster.ru/",
+      "http://api.filmissio.nomoredomainsmonster.ru/",
+      "https://api.filmissio.nomoredomainsmonster.ru/",
+      "http://localhost:3001",
+      "http://localhost:3000",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "origin", "Authorization"],
+  })
 );
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
